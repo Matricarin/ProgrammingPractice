8219d4d054d497d4e0810576d9af2e74c69f2e4c
Matricarin <matricarin@gmail.com>
2023-12-03 16:11:43 +0500

    task1 - added analyzer class for calculate frequency

---
+++ Task1/Task1/Analyzer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Analyzer
+    {
+        private Dictionary<string, int> _dict;
+
+        private double _amountOfWords;
+
+        public Analyzer()
+        {
+            _dict = new Dictionary<string, int>();
+            _amountOfWords = 0;
+        }
+
+        public void AddWord(string word)
+        {
+            if (!_dict.ContainsKey(word))
+            {
+                _dict.Add(word, 1);
+            }
+            else
+            {
+                _dict[word]++;
+            }
+
+            _amountOfWords++;
+        }
+
+        public IEnumerable<(string, double, double)> GetWordsFrequency()
+        {
+            _dict.OrderBy(w => w.Key);
+
+            foreach (var word in _dict)
+            {
+                var freq = word.Value / _amountOfWords;
+                yield return (word.Key, freq, freq * 100);
+            }
+        }
+    }
+}
