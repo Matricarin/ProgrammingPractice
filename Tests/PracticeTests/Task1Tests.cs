6d0229e3eef24dfec7f8d886c16b93d1196a772a
Matricarin <matricarin@gmail.com>
2023-12-03 16:52:23 +0500

    task1 - write first test for GetWordsFrequency

--- Tests/PracticeTests/Task1Tests.cs
+++ Tests/PracticeTests/Task1Tests.cs
@@ -23,5 +23,26 @@ namespace PracticeTests
         {
             Assert.True(true);
         }
+
+        [Test]
+        public void Test_GetWordsFrequency_method_first_value()
+        {
+            var first = _analyzer.GetWordsFrequency().First();
+            Assert.True(first.Item1 == "one" && first.Item2 == (double)1/2 &&
+                        first.Item3 == (double)1/2 * 100);
+        }
+
+        [Test]
+        public void Test_GetWordsFrequency_method_third_value()
+        {
+            var list = new List<(string, double, double)>();
+            foreach (var valueTuple in _analyzer.GetWordsFrequency())
+            {
+                list.Add(valueTuple);
+            }
+            var third = list[3];
+            Assert.True(third.Item1 == "three" && third.Item2 == (double)1/6 &&
+                        third.Item3 == (double)1/6 * 100);
+        }
     }
 }
