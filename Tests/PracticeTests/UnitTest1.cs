1fe86635a483efbe6a625884b405741e7a51a484
Matricarin <matricarin@gmail.com>
2023-12-03 15:15:41 +0500

    Add project files.

---
+++ Tests/PracticeTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PracticeTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
