96cd34093357d429e7f3360b951f05d2239924a5
Matricarin <matricarin@gmail.com>
2023-12-10 17:08:35 +0500

    task1 does not fully read the file

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -19,31 +19,56 @@ namespace Task1
                 using (var stream = File.OpenRead(args[0]))
                 {
                     var reader = new StreamReader(stream);
+                    
+                    encoding = reader.CurrentEncoding;
 
                     var sb = new StringBuilder();
 
-                    while (reader.EndOfStream)
+                    while (true)
                     {
-                        encoding = reader.CurrentEncoding;
-
                         var ch = Convert.ToChar(reader.Read());
 
-                        if (!Char.IsLetterOrDigit(ch))
+                        if (Char.IsLetterOrDigit(ch))
+                        {
+                            var s = ch.ToString().ToLower();
+                            sb.Append(char.Parse(s));
+                        }
+                        else
                         {
                             analyzer.AddWord(sb.ToString());
                             sb.Clear();
                         }
-                        else
+
+                        if (reader.EndOfStream)
                         {
-                            sb.Append(ch);
+                            break;
                         }
                         
                     }
                     reader.Close();
                 }
 
+                var resultAnalysis = analyzer.GetWordsFrequency();
 
+                var resultFileName = args[0].Substring(0, args[0].IndexOf('.')) + "_analyze.csv";
 
+                if (File.Exists(resultFileName))
+                {
+                    File.Delete(resultFileName);
+                }
+
+                using (var stream = File.Create(resultFileName))
+                {
+                    var writer = new StreamWriter(stream, encoding);
+
+                    foreach (var result in resultAnalysis)
+                    {
+                        writer.WriteLine($"{result.Item1}, {result.Item2}, {result.Item3}");
+                    };
+                    writer.Close();
+                }
+
+                Console.WriteLine("Complete.");
             }
             catch (Exception e)
             {
