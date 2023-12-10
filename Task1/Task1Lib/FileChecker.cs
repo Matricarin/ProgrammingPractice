8ba854ac201011293744bb4251c7ea949e2f4ac8
Matricarin <matricarin@gmail.com>
2023-12-10 13:43:16 +0500

    task1 implemented class for check input files

--- Task1/Task1Lib/FileChecker.cs
+++ Task1/Task1Lib/FileChecker.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task1Lib
+﻿namespace Task1Lib
 {
     public class FileChecker
     {
@@ -18,7 +12,31 @@ namespace Task1Lib
 
         private void CheckFiles(string[] files)
         {
+            try
+            {
+                if (files.Length == 0)
+                {
+                    throw new Exception("File names are not entered.");
+                }
 
+                foreach (string file in files)
+                {
+                    var format = file.Contains(".txt");
+                    if (!format)
+                    {
+                        throw new Exception($"Unsupported file format: {file}");
+                    }
+
+                    if (!File.Exists(file))
+                    {
+                        throw new Exception("The file was not found.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
         }
     }
 }
