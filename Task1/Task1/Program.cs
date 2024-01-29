40bb48a9ab190d1a73602bbeac95edc5ce2d6d40
Roman Urmancheev <matricarin@gmail.com>
2024-01-29 07:36:34 +0500

    #5 removed private methods

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using Task1Lib;
 
-using System.Text;
-using System.Globalization;
-
 namespace Task1
 {
     internal static class Program
@@ -15,15 +12,15 @@ namespace Task1
             {
                 var analyzer = new Analyzer();
 
-                var text = OpenAndReadFile(fileName);
+                var text = FileHandler.OpenAndReadFile(fileName);
 
                 analyzer.GetWordsIntoAnalyzerFromText(text);
 
                 var resultAnalysis = analyzer.GetWordsFrequency();
 
-                var resultFileName = GetOutputFileNameWithExtension(fileName, "csv");
+                var resultFileName = FileHandler.GetOutputFileNameWithExtension(fileName, "csv");
 
-                CreateAndWriteResultsInFile(resultAnalysis, resultFileName);
+                FileHandler.CreateAndWriteResultsInFile(resultAnalysis, resultFileName);
 
                 Console.WriteLine("Complete.");
             }
@@ -32,44 +29,5 @@ namespace Task1
                 Console.WriteLine(e.Message);
             }
         }
-
-        private static string OpenAndReadFile(string fileName)
-        {
-            var data = File.ReadAllText(fileName);
-            return data;
-        }
-        
-        private static void CreateAndWriteResultsInFile(IEnumerable<(string Word, double Frequency, double Percent)> results, string fileName)
-        {
-            if (File.Exists(fileName))
-            {
-                File.Delete(fileName);
-            }
-
-            using (var stream = File.Create(fileName))
-            {
-                var encoding = Encoding.UTF8;
-
-                var numberFormatInfo = new NumberFormatInfo()
-                {
-                    NumberDecimalSeparator = "."
-                };
-
-                var writer = new StreamWriter(stream, encoding);
-
-                foreach (var result in results)
-                {
-                    writer.WriteLine($"{result.Word.ToString(numberFormatInfo)}, " +
-                                     $"{result.Frequency.ToString(numberFormatInfo)}, " +
-                                     $"{result.Percent.ToString(numberFormatInfo)}");
-                }
-
-                ;
-                writer.Close();
-            }
-        }
-
-        private static string GetOutputFileNameWithExtension(string fileName, string ext) =>
-            Path.GetFileNameWithoutExtension(fileName) + "_analyze." + $"{ext}";
     }
 }
