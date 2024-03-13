fae6e95ecaee6268aeb0e914d3962e64e1340968
Roman Urmancheev <matricarin@gmail.com>
2024-03-13 16:39:54 +0500

    #13 added new entity for words

---
+++ Common/Common.TasksLibrary/Models/WordWithPercent.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.TasksLibrary.Models
+{
+    sealed class WordWithPercent
+    {
+        public WordWithPercent(string word, double frequency, double percent)
+        {
+            Word = word;
+            Frequency = frequency;
+            Percent = percent;
+        }
+
+        public string Word { get; }
+        public double Frequency { get; }
+        public double Percent { get; }
+    }
+}
