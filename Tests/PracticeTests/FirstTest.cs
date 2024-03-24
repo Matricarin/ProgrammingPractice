c4415188915635296b4a01fe20ca81f16768da1d
matricarin <matricarin@gmail.com>
2024-03-24 12:15:36 +0500

    Added separated check NUnit

---
+++ Tests/PracticeTests/FirstTest.cs
@@ -0,0 +1,12 @@
+namespace PracticeTests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void Test_CheckNUnit()
+        {
+            Assert.True(true);
+        }
+    }
+}
