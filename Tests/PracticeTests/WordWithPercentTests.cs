71673f31ba2380065277f09f5dc81e0940685a70
Roman Urmancheev <matricarin@gmail.com>
2024-03-26 20:22:50 +0500

    change access modifier

--- Tests/PracticeTests/WordWithPercentTests.cs
+++ Tests/PracticeTests/WordWithPercentTests.cs
@@ -3,7 +3,7 @@ using Common.TasksLibrary.Models;
 namespace PracticeTests
 {
     [TestFixture]
-    public class WordWithPercentTests
+    internal class WordWithPercentTests
     {
         private const string Expected = "word, 0.05, 5";
         private WordWithPercent _word;
