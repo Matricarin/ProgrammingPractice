09b41f2ab3dd1b8f0a48a07051be5d7651d7a846
Roman Urmancheev <matricarin@gmail.com>
2024-03-26 20:34:37 +0500

    use parameterixed testfixture

--- Tests/PracticeTests/WordWithPercentTests.cs
+++ Tests/PracticeTests/WordWithPercentTests.cs
@@ -2,16 +2,15 @@ using Common.TasksLibrary.Models;
 
 namespace PracticeTests
 {
-    [TestFixture]
+    [TestFixture("word", 0.05, 5)]
     internal class WordWithPercentTests
     {
         private const string Expected = "word, 0.05, 5";
         private WordWithPercent _word;
-        
-        [SetUp]
-        public void Setup()
+
+        public WordWithPercentTests(string word, double frequency, double percent)
         {
-            _word = new WordWithPercent("word", 0.05, 5);
+            _word = new WordWithPercent(word, frequency, percent);
         }
 
         [Test]
