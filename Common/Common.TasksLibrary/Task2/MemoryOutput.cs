004faee22000c2fe3c62ea4add9c98aaa61989e4
Matricarin <matricarin@gmail.com>
2024-12-02 04:45:26 +0500

    memory output was added

---
+++ Common/Common.TasksLibrary/Task2/MemoryOutput.cs
@@ -0,0 +1,20 @@
+﻿using Common.TasksLibrary.Task2.Base;
+
+namespace Common.TasksLibrary.Task2;
+
+public class MemoryOutput : IOutput
+{
+    private List<double> _valuesStorage;
+
+    public MemoryOutput()
+    {
+        _valuesStorage = new List<double>();
+    }
+
+    public double Value => _valuesStorage.Last();
+
+    public void Post(double data)
+    {
+        _valuesStorage.Add(data);
+    }
+}
