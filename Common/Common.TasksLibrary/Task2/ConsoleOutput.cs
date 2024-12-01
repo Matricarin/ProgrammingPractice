741f5fbf5c08fcc80b422350c4d59f588fe1bff6
Matricarin <matricarin@gmail.com>
2024-12-02 04:45:46 +0500

    console output was added

---
+++ Common/Common.TasksLibrary/Task2/ConsoleOutput.cs
@@ -0,0 +1,16 @@
+﻿using Common.TasksLibrary.Task2.Base;
+
+namespace Common.TasksLibrary.Task2;
+
+public class ConsoleOutput : IOutput
+{
+    public ConsoleOutput()
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+    }
+    
+    public void Post(double data)
+    {
+        Console.Out.WriteLine($"Top stack item is {data}");
+    }
+}
