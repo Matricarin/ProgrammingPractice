ea9afdc5b5fba38f4818f8add66d280b0d2b6c21
Matricarin <matricarin@gmail.com>
2024-12-01 17:29:36 +0500

    test was added

--- Tests/PracticeTests/Task2Tests/CommandsFactoryTest.cs
+++ Tests/PracticeTests/Task2Tests/CommandsFactoryTest.cs
@@ -1,7 +1,31 @@
-﻿namespace PracticeTests.Task2Tests;
+﻿using Common.TasksLibrary.Task2.Base;
 
-[TestFixture]
+namespace PracticeTests.Task2Tests;
+
+[TestFixtureSource(typeof(CommandFactoryCommands), nameof(CommandFactoryCommands.Commands))]
 public class CommandsFactoryTest
 {
-    
+    private Dictionary<string, string> _commandsDict;
+
+    public CommandsFactoryTest(string command, string instance)
+    {
+        
+        _commandsDict = new Dictionary<string, string>();
+        _commandsDict.Add(command, instance);
+    }
+
+    [Test]
+    public void Test_GenerateCommand()
+    {
+        var factory = new CommandsFactory(_commandsDict);
+        var command = factory.GenerateCommand("DEFINE b 5");
+    }
+}
+
+public class CommandFactoryCommands
+{
+    public static object[] Commands =
+    {
+        new object[] { "DEFINE", "Common.TasksLibrary.Task2.Commands.DefineCommand" }
+    };
 }
