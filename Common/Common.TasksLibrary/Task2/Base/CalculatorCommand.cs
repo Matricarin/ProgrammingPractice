31732fd6ebb5594ec9d0e44402b974211089393e
Matricarin <matricarin@gmail.com>
2024-12-01 15:31:41 +0500

    abstract command was added

---
+++ Common/Common.TasksLibrary/Task2/Base/CalculatorCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.TasksLibrary.Task2.Base;
+
+public abstract class CalculatorCommand
+{
+    protected string Variable { get; init; } 
+    protected double Value { get; init; }
+    public abstract void Process(Calculator calculator);
+}
