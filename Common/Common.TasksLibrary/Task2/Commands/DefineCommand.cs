17c701ceb93625c6db1b336fca6316e692b75049
Matricarin <matricarin@gmail.com>
2024-12-01 15:32:35 +0500

    first command was added

---
+++ Common/Common.TasksLibrary/Task2/Commands/DefineCommand.cs
@@ -0,0 +1,15 @@
+﻿using Common.TasksLibrary.Task2.Base;
+using static System.Exception;
+
+namespace Common.TasksLibrary.Task2.Commands;
+
+public sealed class DefineCommand : CalculatorCommand
+{
+    public override void Process(Calculator calculator)
+    {
+        if (!calculator.VariablesStorage.TryAdd(base.Variable, base.Value))
+        {
+            throw new Exception($"Variable \"{base.Variable}\" was defined earlier");
+        }
+    }
+}
