de6ac9fb37e2deaf2de3d667bfb021dbdc39c52e
Matricarin <matricarin@gmail.com>
2024-12-01 17:29:54 +0500

    refactored

--- Common/Common.TasksLibrary/Task2/Commands/DefineCommand.cs
+++ Common/Common.TasksLibrary/Task2/Commands/DefineCommand.cs
@@ -5,11 +5,21 @@ namespace Common.TasksLibrary.Task2.Commands;
 
 public sealed class DefineCommand : CalculatorCommand
 {
+    private string _variableName;
+    private double _variableValue;
+    public DefineCommand(string name, string value)
+    {
+        _variableName = name;
+        if (!double.TryParse(value, out _variableValue))
+        {
+            throw new Exception("Variable value was not a number.");
+        }
+    }
     public override void Process(Calculator calculator)
     {
-        if (!calculator.VariablesStorage.TryAdd(base.Variable, base.Value))
+        if (!calculator.VariablesStorage.TryAdd(_variableName, _variableValue))
         {
-            throw new Exception($"Variable \"{base.Variable}\" was defined earlier");
+            throw new Exception($"Variable \"{_variableName}\" was defined earlier");
         }
     }
 }
