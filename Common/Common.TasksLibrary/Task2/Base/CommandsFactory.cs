2f96422becf5c687e145ddb45f5c7a364f17e926
Matricarin <matricarin@gmail.com>
2024-12-01 15:31:12 +0500

    commands factory was added

---
+++ Common/Common.TasksLibrary/Task2/Base/CommandsFactory.cs
@@ -0,0 +1,28 @@
+﻿using Common.TasksLibrary.Task2.Commands;
+
+namespace Common.TasksLibrary.Task2.Base;
+
+public sealed class CommandsFactory
+{
+    private static Dictionary<string, string> _commandMapper;
+
+    public CommandsFactory(Dictionary<string, string> commands)
+    {
+        _commandMapper = commands;
+    }
+
+    public CalculatorCommand GenerateCommand(string executingCommand)
+    {
+        try
+        {
+            var instance = Activator.CreateInstance(null, 
+                _commandMapper[executingCommand]);
+            var command = (DefineCommand)instance.Unwrap();
+            return command;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
