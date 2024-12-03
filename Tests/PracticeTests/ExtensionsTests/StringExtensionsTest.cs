10cf98252d8caefadeada9562455ec3fa613cc4f
Matricarin <matricarin@gmail.com>
2024-12-03 21:11:22 +0500

    new tests were added

---
+++ Tests/PracticeTests/ExtensionsTests/StringExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Common.TasksLibrary.Extensions;
+
+namespace PracticeTests.ExtensionsTests;
+
+[TestFixture]
+public class StringExtensionsTest
+{
+    [TestCase("SubStringBeforeTo", "Before", "SubString")]
+    [TestCase("SubStringBeforeToBeforeTo", "Before", "SubString")]
+    [TestCase("SubStringBeforeTo", "Sub", "")]
+    [TestCase("SubStringBeforeTo", "", "")]
+    public void Test_SubStringBeforeTo(string source, string separator, string expected)
+    {
+        Assert.That(source.SubStringBeforeFirstOne(separator), Is.EqualTo(expected));
+    }
+    [TestCase("SubStringAfterThat", "After", "That")]
+    [TestCase("SubStringAfterThatAfterThat", "After", "ThatAfterThat")]
+    [TestCase("SubStringAfterThat", "That", "")]
+    [TestCase("SubStringAfterThat", "", "")]
+    public void Test_SubStringAfterThat(string source, string separator, string expected)
+    {
+        Assert.That(source.SubStringAfterFirstOne(separator), Is.EqualTo(expected));
+    }
+}
