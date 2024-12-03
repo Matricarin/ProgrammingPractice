f246dd7412825c121ed26f6ad05632809434d1e1
Matricarin <matricarin@gmail.com>
2024-12-03 21:11:05 +0500

    new extensions were added

---
+++ Common/Common.TasksLibrary/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using static Common.TasksLibrary.Constants.IntegersConstants;
+
+namespace Common.TasksLibrary.Extensions;
+
+public static class StringExtensions
+{
+    public static string SubStringBeforeFirstOne(this string source, string separator)
+    {
+        if (string.IsNullOrEmpty(separator))
+        {
+            return string.Empty;
+        }
+        var index = source.IndexOf(separator, StringComparison.Ordinal);
+        return source.Substring(Zero, index);
+    }
+
+    public static string SubStringAfterFirstOne(this string source, string separator)
+    {
+        if (string.IsNullOrEmpty(separator))
+        {
+            return string.Empty;
+        }
+        var index = source.IndexOf(separator, StringComparison.Ordinal);
+        return source.Substring(index + separator.Length);
+    }
+}
