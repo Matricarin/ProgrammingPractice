e7a078b13ea102f01527b221ac295581a97af44f
Matricarin <matricarin@gmail.com>
2024-12-08 07:42:54 +0500

    builder was created for simply initialization

---
+++ Common/Common.TasksLibrary/Task2/Base/CalculatorBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.TasksLibrary.Task2.Base;
+
+public sealed class CalculatorBuilder
+{
+    public IOutput CalculatorOutput { get; set; }
+    
+    public static void Build()
+    {
+    }
+
+    private void ConfigureFactory()
+    {
+    }
+}
