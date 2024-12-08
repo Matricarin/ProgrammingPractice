e31729e2cee9ddb85cc5b0db78e6fcd011a765b0
Matricarin <matricarin@gmail.com>
2024-12-08 07:55:01 +0500

    command in list was added

--- Common/Common.TasksLibrary/Task2/Base/CommandsList.cs
+++ Common/Common.TasksLibrary/Task2/Base/CommandsList.cs
@@ -2,5 +2,14 @@
 
 public class CommandsList
 {
-    
+    public string DefineCommand { get; }
+    public string PopCommand { get; }
+    public string PushCommand { get; }
+    public string AdditionCommand { get; }
+    public string SubtractionCommand { get; }
+    public string MultiplicationCommand { get; }
+    public string DevisionCommand { get; }
+    public string SqrtCommand { get; }
+    public string PrintCommand { get; }
+    public string CommentCommand { get; }
 }
