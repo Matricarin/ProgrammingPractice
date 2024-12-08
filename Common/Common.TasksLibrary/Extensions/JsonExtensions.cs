a6ef9a0e0fe0cbe020208f6657bf8e296c190d8d
Matricarin <matricarin@gmail.com>
2024-12-08 07:55:17 +0500

    json serialization was added

---
+++ Common/Common.TasksLibrary/Extensions/JsonExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+
+namespace Common.TasksLibrary.Extensions;
+
+public static class JsonExtensions
+{
+    public static T? FromJsonString<T>(this string jsonString)
+    {
+        return JsonSerializer.Deserialize<T>(jsonString);
+    }
+
+    public static string ToJson<T>(this T item)
+    {
+        var options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin,
+                UnicodeRanges.Cyrillic, UnicodeRanges.LetterlikeSymbols)
+        };
+        return JsonSerializer.Serialize<T>(item, options);
+    }
+
+    public static T? FromJsonFile<T>(this FileInfo file)
+    {
+        var jsonString = File.ReadAllText(file.FullName);
+        return jsonString.FromJsonString<T>();
+    }
+}
