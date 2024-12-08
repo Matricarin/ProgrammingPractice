b56852642f074a78e1e3ebc6119c5a573abcba7c
Matricarin <matricarin@gmail.com>
2024-12-08 21:32:18 +0500

    refactored

--- Common/Common.TasksLibrary/Task2/Base/CommandsFactory.cs
+++ Common/Common.TasksLibrary/Task2/Base/CommandsFactory.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Common.TasksLibrary.Constants;
+﻿using Common.TasksLibrary.Constants;
 using Common.TasksLibrary.Extensions;
 
 namespace Common.TasksLibrary.Task2.Base;
@@ -19,9 +18,9 @@ public sealed class CommandsFactory
         {
             var stringCommand = executingCommand.SubStringBeforeFirstOne(StringConstants.WhiteSpace);
             var stringParameters = executingCommand.SubStringAfterFirstOne(StringConstants.WhiteSpace);
-            var instance = Activator.CreateInstance(Type.GetType(_commandsList.GetType().GetProperties()
-                    .First(s => s.Name == stringCommand).GetValue(_commandsList).ToString()), 
-                    stringParameters);
+            var instance = Activator.CreateInstance(
+                Type.GetType(_commandsList.GetType().GetProperties().First(s => s.Name == stringCommand)
+                    .GetValue(_commandsList).ToString()), stringParameters);
             var command = (CalculatorCommand)instance;
             return command;
         }
