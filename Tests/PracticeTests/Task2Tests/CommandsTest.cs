b12562303095bb89432def07c2ac3f9672420d62
Matricarin <matricarin@gmail.com>
2024-12-08 06:37:45 +0500

    new test was added

--- Tests/PracticeTests/Task2Tests/CommandsTest.cs
+++ Tests/PracticeTests/Task2Tests/CommandsTest.cs
@@ -1,6 +1,12 @@
 ﻿namespace PracticeTests.Task2Tests;
 
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public class CommandsTest
 {
-    
+    [Ignore("I am thinking about calculator incance creation")]
+    public void Test_DefineCommand_Process()
+    {
+        Assert.That(false, Is.EqualTo(true));
+    }
 }
