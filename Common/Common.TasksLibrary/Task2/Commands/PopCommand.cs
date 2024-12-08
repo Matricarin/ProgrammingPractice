949b644cc47c0cdf7475742529e72e96ad62cb97
Matricarin <matricarin@gmail.com>
2024-12-08 21:31:48 +0500

    new command was added

---
+++ Common/Common.TasksLibrary/Task2/Commands/PopCommand.cs
@@ -0,0 +1,35 @@
+﻿using Common.TasksLibrary.Constants;
+using Common.TasksLibrary.Task2.Base;
+
+namespace Common.TasksLibrary.Task2.Commands;
+
+public class PopCommand : CalculatorCommand
+{
+    private string _variableName;
+
+    public PopCommand(string parameters)
+    {
+        if (string.IsNullOrWhiteSpace(parameters))
+        {
+            throw new Exception("Unexpected parameter value.");
+        }
+        var values = parameters.Split(CharsConstants.WhiteSpace);
+        if (values.Length > 1)
+        {
+            throw new Exception("Unexpected amount of parameters for command.");
+        }
+        _variableName = parameters;
+    }
+    public override void Process(Calculator calculator)
+    {
+        try
+        {
+            var topValue = calculator.StackStorage.Pop();
+            calculator.VariablesStorage.TryAdd(_variableName, topValue);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
