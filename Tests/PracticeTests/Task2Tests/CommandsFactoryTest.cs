92784c67ad3970d4f7da61603d11c8b86069c0ac
Matricarin <matricarin@gmail.com>
2024-12-11 18:05:19 +0500

    generate test was added

--- Tests/PracticeTests/Task2Tests/CommandsFactoryTest.cs
+++ Tests/PracticeTests/Task2Tests/CommandsFactoryTest.cs
@@ -1,12 +1,30 @@
-﻿namespace PracticeTests.Task2Tests;
+﻿using Common.TasksLibrary.Task2.Base;
+using Common.TasksLibrary.Task2.Commands;
+
+namespace PracticeTests.Task2Tests;
 
 [TestFixture]
 [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public class CommandsFactoryTest
 {
-    
-    public void Test_GenerateCommand(string stringCommand, string place)
+    private CommandsList _commands = new CommandsList()
     {
-
+        DEFINE = "Common.TasksLibrary.Task2.Commands.DefineCommand",
+    };
+    
+    [TestCaseSource(typeof(CommandsFactoryTestData), nameof(CommandsFactoryTestData.GenerateDefineCommand))]
+    public void Test_GenerateDefineCommand(string stringCommand, DefineCommand expected)
+    {
+        var factory = new CommandsFactory(_commands);
+        var result = factory.GenerateCommand(stringCommand);
+        Assert.That(result, Is.EqualTo(expected));
     }
+}
+
+public static class CommandsFactoryTestData
+{
+    public static object[] GenerateDefineCommand = new object[]
+    {
+        new object[] {"DEFINE a 5", new DefineCommand("a 5")},
+    };
 }
