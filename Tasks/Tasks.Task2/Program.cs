c641f035b228f117f7fb91798446c54993a0e1a4
Matricarin <matricarin@gmail.com>
2024-12-11 20:13:36 +0500

    top-down approach was started

--- Tasks/Tasks.Task2/Program.cs
+++ Tasks/Tasks.Task2/Program.cs
@@ -4,6 +4,14 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //read args
+        
+        //check file
+
+        //use console or read file
+        
+        //after executing commands fom file listen console input
+        
+        //add command to finish program execution
     }
 }
