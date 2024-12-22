b0a28c188be9b5f5ab3c9ccc51beeda3f530806e
Matricarin <matricarin@gmail.com>
2024-12-22 22:23:19 +0500

    main logic was changed

--- Common/Common.TasksLibrary/Task2/Base/CommandsFactory.cs
+++ Common/Common.TasksLibrary/Task2/Base/CommandsFactory.cs
@@ -1,28 +1,25 @@
 ﻿using Common.TasksLibrary.Constants;
 using Common.TasksLibrary.Extensions;
+using Common.TasksLibrary.Task2.Commands;
 
 namespace Common.TasksLibrary.Task2.Base;
 
 public sealed class CommandsFactory
 {
-    private static CommandsList _commandsList;
-
-    public CommandsFactory(CommandsList commands)
-    {
-        _commandsList = commands;
-    }
-
     public CalculatorCommand GenerateCommand(string executingCommand)
     {
         try
         {
             var stringCommand = executingCommand.SubStringBeforeFirstOne(StringConstants.WhiteSpace);
             var stringParameters = executingCommand.SubStringAfterFirstOne(StringConstants.WhiteSpace);
+            /*
             var instance = Activator.CreateInstance(
                 Type.GetType(_commandsList.GetType().GetProperties().First(s => s.Name == stringCommand)
                     .GetValue(_commandsList).ToString()), stringParameters);
-            var command = (CalculatorCommand)instance;
-            return command;
+            */
+            // var command = (CalculatorCommand)instance;
+            // return command;
+            return new DefineCommand(string.Empty);
         }
         catch (Exception e)
         {
