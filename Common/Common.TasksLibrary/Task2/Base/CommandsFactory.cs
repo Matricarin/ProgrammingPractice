6e37858d78e26301d76a5bb480e0b6339db9a384
Matricarin <matricarin@gmail.com>
2025-01-03 08:19:52 +0500

    factory was fixed

--- Common/Common.TasksLibrary/Task2/Base/CommandsFactory.cs
+++ Common/Common.TasksLibrary/Task2/Base/CommandsFactory.cs
@@ -1,4 +1,5 @@
-﻿using Common.TasksLibrary.Constants;
+﻿using System.Reflection;
+using Common.TasksLibrary.Constants;
 using Common.TasksLibrary.Extensions;
 using Common.TasksLibrary.Task2.Commands;
 
@@ -12,14 +13,11 @@ public sealed class CommandsFactory
         {
             var stringCommand = executingCommand.SubStringBeforeFirstOne(StringConstants.WhiteSpace);
             var stringParameters = executingCommand.SubStringAfterFirstOne(StringConstants.WhiteSpace);
-            /*
-            var instance = Activator.CreateInstance(
-                Type.GetType(_commandsList.GetType().GetProperties().First(s => s.Name == stringCommand)
-                    .GetValue(_commandsList).ToString()), stringParameters);
-            */
-            // var command = (CalculatorCommand)instance;
-            // return command;
-            return new DefineCommand(string.Empty);
+            
+            var instance = Activator.CreateInstance(Type.GetType(stringCommand), stringParameters);
+            
+            var command = (CalculatorCommand)instance;
+            return command;
         }
         catch (Exception e)
         {
