4bdc9135714787a1fb0a3ba26a11d63d1a1f933a
Matricarin <matricarin@gmail.com>
2025-01-03 08:39:12 +0500

    new project

---
+++ Tasks/Tasks.Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Common.TasksLibrary.Task2;
+using Common.TasksLibrary.Task2.Base;
+
+namespace Tasks.Task2;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var fileInfo = new FileInfo(args.First());
+
+        var calculator = new CalculatorBuilder
+            {
+                OutputOptions = CalculatorOutputOptions.Console
+            }
+            .Build();
+        
+        ExecuteCommandsFromFile(fileInfo, calculator);
+        
+        ExecuteCommandsFromConsoleInput(calculator);
+    }
+
+    private static void ExecuteCommandsFromFile(FileInfo fileInfo, Calculator calculator)
+    {
+        try
+        {
+            var commands = File.ReadAllLines(fileInfo.FullName);
+            
+            calculator.Execute(commands);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    private static void ExecuteCommandsFromConsoleInput(Calculator calculator)
+    {
+        while (true)
+        {
+            var command = Console.ReadLine();
+            
+            calculator.Execute(command);
+        }
+    }
+}
