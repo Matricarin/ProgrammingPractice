5945a5137b69a03bf4a4ca19d7c7941e2cc427cb
Matricarin <matricarin@gmail.com>
2025-01-05 12:07:36 +0500

    new command was added

---
+++ Common/Common.TasksLibrary/Task2/Commands/MultiplyCommand.cs
@@ -0,0 +1,33 @@
+﻿using Common.TasksLibrary.Task2.Base;
+
+namespace Common.TasksLibrary.Task2.Commands;
+
+public class MultiplyCommand : CalculatorCommand
+{
+    public MultiplyCommand(string parameters)
+    {
+        if (!string.IsNullOrEmpty(parameters))
+        {
+            throw new Exception("Subtract operation can't have command parameters");
+        }
+    }
+
+    public MultiplyCommand()
+    {
+        
+    }
+    public override void Process(Calculator calculator)
+    {
+        try
+        {
+            var first = calculator.StackStorage.Pop();
+            var second = calculator.StackStorage.Pop();
+            var result = first * second;
+            calculator.StackStorage.Push(result);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
