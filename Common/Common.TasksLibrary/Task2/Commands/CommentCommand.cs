431f4354a45ebfce672d3b493e722a7fdee903e1
Matricarin <matricarin@gmail.com>
2025-01-05 12:20:19 +0500

    new command was added

--- Common/Common.TasksLibrary/Task2/Commands/CommentCommand.cs
+++ Common/Common.TasksLibrary/Task2/Commands/CommentCommand.cs
@@ -5,22 +5,14 @@ namespace Common.TasksLibrary.Task2.Commands;
 public class CommentCommand : CalculatorCommand
 {
     private string _comment;
+    
     public CommentCommand(string parameters)
     {
-        var values = parameters.Split(CharsConstants.WhiteSpace);
-        if (values.Length != 2)
-        {
-            throw new Exception("Unexpected amount of parameters for command");
-        }
-
-        _variableName = values.First();
-        if (!double.TryParse(values.Last(), out _variableValue))
-        {
-            throw new Exception("Unexpected value for command parameter");
-        }
+        _comment = parameters;
     }
+    
     public override void Process(Calculator calculator)
     {
-        throw new NotImplementedException();
+        // what do I with comment?
     }
 }
