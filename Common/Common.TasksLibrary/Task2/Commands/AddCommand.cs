a75998f879ed87ef83612671041ddf16b40b1178
Matricarin <matricarin@gmail.com>
2025-01-05 11:50:23 +0500

    new command was added

--- Common/Common.TasksLibrary/Task2/Commands/AddCommand.cs
+++ Common/Common.TasksLibrary/Task2/Commands/AddCommand.cs
@@ -1,6 +1,33 @@
-﻿namespace Common.TasksLibrary.Task2.Commands;
+﻿using Common.TasksLibrary.Task2.Base;
 
-public class AddCommand
+namespace Common.TasksLibrary.Task2.Commands;
+
+public class AddCommand : CalculatorCommand
 {
-    
+    public AddCommand(string parameters)
+    {
+        if (!string.IsNullOrEmpty(parameters))
+        {
+            throw new Exception("Adding operation can't have command parameters");
+        }
+    }
+
+    public AddCommand()
+    {
+        
+    }
+    public override void Process(Calculator calculator)
+    {
+        try
+        {
+            var first = calculator.StackStorage.Pop();
+            var second = calculator.StackStorage.Pop();
+            var result = first + second;
+            calculator.StackStorage.Push(result);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
 }
