067103d03dfac0b64badaa38ce9097f02aa1c48b
Matricarin <matricarin@gmail.com>
2025-02-12 15:06:36 +0500

    new tests were added

--- Tests/PracticeTests/Task2Tests/CommandsExecutionTests.cs
+++ Tests/PracticeTests/Task2Tests/CommandsExecutionTests.cs
@@ -9,27 +9,87 @@ namespace PracticeTests.Task2Tests;
 [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public class CommandsExecutionTests
 {
-    private CalculatorExecutionContext _context;
     private MockContainer _container;
+    private CalculatorExecutionContext _context;
     private const double FirstNumber = 10.1;
     private const double SecondNumber = 20.2;
-    private const double Result = 30.3;
-    
-    [SetUp]
-    public void Setup()
+    private const double AdditionResult = 30.3;
+    private const double SubtractionResult = 10.1;
+    private const double MultiplicationResult = 204.02;
+    private const double DivisionResult = 2.0;
+    private const double SqrtResult = 3.17804971641414;
+
+    [Test]
+    public void Test_AddCommandExecution()
     {
         _container = new MockContainer();
         _context = new CalculatorExecutionContext(new ConsoleOutput(), _container);
         _context.Push(FirstNumber);
         _context.Push(SecondNumber);
-    }
 
-    [Test]
-    public void Test_AddCommandExecution()
-    {
         var command = new AddCommand(string.Empty);
         command.Process(_context);
         var result = _context.Pop();
-        Assert.True(Math.Abs(result - Result) < DoublesConstants.Tolerance);
+
+        Assert.That(Math.Abs(result - AdditionResult), Is.LessThan(DoublesConstants.Tolerance));
+    }
+
+    [Test]
+    public void Test_SubtractCommandExecution()
+    {
+        _container = new MockContainer();
+        _context = new CalculatorExecutionContext(new ConsoleOutput(), _container);
+        _context.Push(FirstNumber);
+        _context.Push(SecondNumber);
+
+        var command = new SubtractCommand(string.Empty);
+        command.Process(_context);
+        var result = _context.Pop();
+
+        Assert.That(Math.Abs(result - SubtractionResult), Is.LessThan(DoublesConstants.Tolerance));
+    }
+
+    [Test]
+    public void Test_MultiplyCommandExecution()
+    {
+        _container = new MockContainer();
+        _context = new CalculatorExecutionContext(new ConsoleOutput(), _container);
+        _context.Push(FirstNumber);
+        _context.Push(SecondNumber);
+
+        var command = new MultiplyCommand(string.Empty);
+        command.Process(_context);
+        var result = _context.Pop();
+
+        Assert.That(Math.Abs(result - MultiplicationResult), Is.LessThan(DoublesConstants.Tolerance));
+    }
+    
+    [Test]
+    public void Test_DivideCommandExecution()
+    {
+        _container = new MockContainer();
+        _context = new CalculatorExecutionContext(new ConsoleOutput(), _container);
+        _context.Push(FirstNumber);
+        _context.Push(SecondNumber);
+
+        var command = new DivideCommand(string.Empty);
+        command.Process(_context);
+        var result = _context.Pop();
+
+        Assert.That(Math.Abs(result - DivisionResult), Is.LessThan(DoublesConstants.Tolerance));
+    }
+    
+    [Test]
+    public void Test_SqrtCommandExecution()
+    {
+        _container = new MockContainer();
+        _context = new CalculatorExecutionContext(new ConsoleOutput(), _container);
+        _context.Push(FirstNumber);
+
+        var command = new SqrtCommand(string.Empty);
+        command.Process(_context);
+        var result = _context.Pop();
+
+        Assert.That(Math.Abs(result - SqrtResult), Is.LessThan(DoublesConstants.Tolerance));
     }
 }
