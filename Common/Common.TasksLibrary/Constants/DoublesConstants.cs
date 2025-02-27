74ff6e2e6e07359c1d19b2856b1e3745612aca12
Matricarin <matricarin@gmail.com>
2025-02-27 18:21:13 +0500

    unnecessary constants were deleted

--- Common/Common.TasksLibrary/Constants/DoublesConstants.cs
+++ Common/Common.TasksLibrary/Constants/DoublesConstants.cs
@@ -2,10 +2,6 @@
 {
     public static class DoublesConstants
     {
-        public const double Half = 0.5;
-        public const double One = 1.0;
-        public const double OneAndHalf = 1.5;
         public const double Tolerance = 0.000001;
-        public const double BigTolerance = 0.001;
     }
 }
