dd0da2f03690606a6771a8573450378816d9bb37
Matricarin <matricarin@gmail.com>
2025-02-27 18:31:22 +0500

    unnecessary constants were removed

--- Common/Common.TasksLibrary/Constants/IntegersConstants.cs
+++ Common/Common.TasksLibrary/Constants/IntegersConstants.cs
@@ -2,10 +2,6 @@
 {
     public static class IntegersConstants
     {
-        public const int Zero = 0;
-        public const int One = 1;
-        public const int Two = 2;
-        public const int Three = 3;
-        public const int OneHundred = 100;
+        public const int MaxPercent = 100;
     }
 }
