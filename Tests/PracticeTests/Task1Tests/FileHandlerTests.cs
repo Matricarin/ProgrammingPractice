db5b84575e3fb82bb4c8532d8527994da3dfebea
Matricarin <matricarin@gmail.com>
2025-03-01 08:11:47 +0500

    file handler was tested

--- Tests/PracticeTests/Task1Tests/FileHandlerTests.cs
+++ Tests/PracticeTests/Task1Tests/FileHandlerTests.cs
@@ -1,42 +1,36 @@
-﻿using Common.TasksLibrary;
-using Common.TasksLibrary.Constants;
-using Common.TasksLibrary.Task1;
+﻿using Common.TasksLibrary.Task1;
 
 namespace PracticeTests.Task1Tests;
-[TestFixture]
+[TestFixture, FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public class FileHandlerTests
 {
-   private FileInfo _testFile;
-   private string _readContentInFile;
-   private List<WordWithPercent> _testWords;
-   private string _contentInResultFile;
-   [SetUp]
-   public void Setup()
-   {
-      _testFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, 
-         "TestingData","Task1_FileHandlerTest_OpenAndRead.txt"));
-      _readContentInFile = "Hello from file!";
-      _testWords = new List<WordWithPercent>()
-      {
-         new WordWithPercent("Hello", 0.1, 10),
-         new WordWithPercent("file", 0.9, 90)
-      };
-      _contentInResultFile = "Hello, 0.1, 10\r\nfile, 0.9, 90\r\n";
-   }
-
    [Test]
    public void OpenAndReadFile()
    {
-      var result = FileHandler.OpenAndReadFile(_testFile);
-      Assert.That(result, Is.EqualTo(_readContentInFile));
+      var testFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, 
+         "TestingData","Task1_FileHandlerTest_OpenAndRead.txt"));
+      
+      var readContentInFile = "Hello from file!";
+      
+      var handler = new FileHandler(testFile);
+      
+      var result =handler.OpenAndReadFile();
+      
+      Assert.That(result, Is.EqualTo(readContentInFile));
    }
-
+   
    [Test]
-   public void CreateAndWriteCsvFile()
+   public void CreateHandlerWithException()
    {
-      FileHandler.CreateAndWriteResultsInCsvFile(_testWords);
-      var content = File.ReadAllText(StringConstants.ResultCsvFileName);
-      Assert.That(content, Is.EqualTo(_contentInResultFile));
+      var expected = new Exception("Please provide a file.");
+      try
+      {
+         var handler = new FileHandler(null);
+      }
+      catch (Exception e)
+      {
+         Assert.That(expected.Message, Is.EqualTo(e.Message));
+      }
    }
 }
 
