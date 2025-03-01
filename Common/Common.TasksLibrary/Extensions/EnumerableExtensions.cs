3d5d39962ec11680bf0a77144592b36790a59070
Matricarin <matricarin@gmail.com>
2025-03-01 08:11:02 +0500

    new extension was added

---
+++ Common/Common.TasksLibrary/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Common.TasksLibrary.Task1;
+
+namespace Common.TasksLibrary.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static void CreateAndWriteResultsInCsvFile(this IEnumerable<WordWithPercent> results)
+    {
+        using (var stream = File.Create(Constants.StringConstants.ResultCsvFileName))
+        {
+            var encoding = Encoding.UTF8;
+                
+            var writer = new StreamWriter(stream, encoding);
+
+            foreach (var result in results)
+            {
+                writer.WriteLine(result.ToString());
+            }
+
+            writer.Close();
+        }
+    }
+}
