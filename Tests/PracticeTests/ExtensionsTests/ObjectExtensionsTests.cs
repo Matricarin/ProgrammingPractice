4f14759cbddce440562acef7826aacd3640169b9
Matricarin <matricarin@gmail.com>
2025-03-01 10:23:58 +0500

    new tests were added

---
+++ Tests/PracticeTests/ExtensionsTests/ObjectExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Common.TasksLibrary.Extensions;
+
+namespace PracticeTests.ExtensionsTests;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public sealed class ObjectExtensionsTests
+{
+    [Test]
+    public void IsNullShouldReturnTrue()
+    {
+        object? obj = null;
+        Assert.True(obj.IsNull());
+    }
+    [Test]
+    public void IsNullShouldReturnFalse()
+    {
+        var obj = new object();
+        Assert.False(obj.IsNull());
+    }
+}
