a0fe119f59a4a005513d7f563f2065955b53ba39
Matricarin <matricarin@gmail.com>
2025-03-01 08:10:39 +0500

    class was refactored

--- Common/Common.TasksLibrary/Task1/FileHandler.cs
+++ Common/Common.TasksLibrary/Task1/FileHandler.cs
@@ -1,30 +1,21 @@
-﻿using System.Text;
-
-namespace Common.TasksLibrary.Task1
+﻿namespace Common.TasksLibrary.Task1
 {
-    public static class FileHandler
+    public class FileHandler
     {
-        public static string OpenAndReadFile(FileInfo fileInfo)
+        private FileInfo _fileInfo;
+        public FileHandler(FileInfo fileInfo)
         {
-            var data = File.ReadAllText(fileInfo.FullName);
-            return data;
-        }
-
-        public static void CreateAndWriteResultsInCsvFile(IEnumerable<WordWithPercent> results)
-        {
-            using (var stream = File.Create(Constants.StringConstants.ResultCsvFileName))
+            if (fileInfo == null)
             {
-                var encoding = Encoding.UTF8;
-                
-                var writer = new StreamWriter(stream, encoding);
-
-                foreach (var result in results)
-                {
-                    writer.WriteLine(result.ToString());
-                }
-
-                writer.Close();
+                throw new NullReferenceException("Please provide a file.");
             }
+            
+            _fileInfo = fileInfo;
+        }
+        public string OpenAndReadFile()
+        {
+            var data = File.ReadAllText(_fileInfo.FullName);
+            return data;
         }
     }
 }
