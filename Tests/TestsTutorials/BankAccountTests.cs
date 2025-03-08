2954f470b1398d349afd09a4ecb244cd201f8dbd
Matricarin <matricarin@gmail.com>
2025-03-08 11:38:33 +0500

    new tests were added

---
+++ Tests/TestsTutorials/BankAccountTests.cs
@@ -0,0 +1,21 @@
+﻿using TestsTutorials.Models;
+
+namespace TestsTutorials;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public sealed class BankAccountTests
+{
+    private BankAccount bankAccount = new BankAccount(new ConsoleLog())
+    {
+        Balance = 100
+    };
+
+    [Test]
+    public void DepositIntergrationTest()
+    {
+        bankAccount.Deposit(100);
+        
+        Assert.That(bankAccount.Balance, Is.EqualTo(200));
+    }
+}
---
+++ Tests/TestsTutorials/Models/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace TestsTutorials.Models;
+
+public interface ILog
+{
+    void Log(string message);
+}
+
+public sealed class ConsoleLog : ILog
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+public sealed class BankAccount
+{
+    private readonly ILog _log;
+    public decimal Balance { get; set; }
+
+    public BankAccount(ILog log)
+    {
+        _log = log;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+}
