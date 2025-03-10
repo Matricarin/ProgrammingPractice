4168784bfeb2372b55189f5c9bb348bfac066429
Roman Urmancheev <matricarin@gmail.com>
2025-03-10 18:02:17 +0500

    calculator container was been refactored

--- Common/Common.TasksLibrary/Task2/Output/CalculatorContainer.cs
+++ Common/Common.TasksLibrary/Task2/Output/CalculatorContainer.cs
@@ -2,14 +2,8 @@
 
 namespace Common.TasksLibrary.Task2.Output;
 
-public class CalculatorContainer : BaseContainer
+public sealed class CalculatorContainer : BaseContainer
 {
-    public CalculatorContainer()
-    {
-        VariableStorage = new Dictionary<string, double>();
-        Stack = new Stack<double>();
-    }
-
-    public override Dictionary<string, double> VariableStorage { get; set; }
-    public override Stack<double> Stack { get; set; }
+    public override Dictionary<string, double> VariableStorage { get; set; } = new();
+    public override Stack<double> Stack { get; set; } = new();
 }
