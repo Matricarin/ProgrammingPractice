277a26736a6cf3bca2e44acde0d38fd744ba889e
Matricarin <matricarin@gmail.com>
2025-03-11 19:56:42 +0500

    new parsing process was added

---
+++ Common/Common.TasksLibrary/Task2/Handlers/CommandParser.cs
@@ -0,0 +1,33 @@
+﻿namespace Common.TasksLibrary.Task2.Handlers;
+
+sealed class CommandParser
+{
+    public string Command { get; }
+    public string Arguments { get; }
+
+    public CommandParser(string command)
+    {
+        if (!string.IsNullOrWhiteSpace(command))
+        {
+            throw new ArgumentException("Command cannot be empty.", nameof(command));
+        }
+        
+        var spannedCommand = command.AsSpan();
+        var whiteSpaceIndex = spannedCommand.IndexOf(' ');
+        
+        if (whiteSpaceIndex == -1)
+        {
+            Command = spannedCommand.ToString();
+            Arguments = string.Empty;
+        }
+        else
+        {
+            spannedCommand.Slice(0, whiteSpaceIndex);
+            Command = spannedCommand.ToString();
+
+            spannedCommand.Slice(whiteSpaceIndex + 1);
+            Arguments = spannedCommand.ToString();
+        }
+        
+    }
+}
