d63d0fc86895f50c765b190709c63217e40223ec
Matricarin <matricarin@gmail.com>
2025-03-11 20:11:08 +0500

    new attribute was added

---
+++ Common/Common.TasksLibrary/Task2/Attributes/CommandSignedAsAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.TasksLibrary.Task2.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class CommandSignedAsAttribute : Attribute
+{
+    public string Sign { get; }
+    public CommandSignedAsAttribute(string sign) => Sign = sign;
+}
