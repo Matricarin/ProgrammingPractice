82f11a9e84c960f13de9336ea4090eaae7f55bb8
Matricarin <matricarin@gmail.com>
2025-03-12 18:31:48 +0500

    new tests were added

---
+++ Tests/PracticeTests/Task2Tests/CommandParserTests.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticeTests.Task2Tests;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public sealed class CommandParserTests
+{
+    [Test]
+    public void ParseCommandTest()
+    {
+        Assert.IsTrue(true);
+    }
+}
---
+++ Tests/PracticeTests/Task2Tests/CommandsFactoryTests.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticeTests.Task2Tests;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public sealed class CommandsFactoryTests
+{
+    [Test]
+    public void GenerateAddCommandTest()
+    {
+        Assert.IsTrue(true);
+    }
+}
