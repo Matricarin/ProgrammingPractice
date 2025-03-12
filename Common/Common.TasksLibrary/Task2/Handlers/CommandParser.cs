03de3e4d3f954a42a21b0a0c62e8b4619ceb7573
Matricarin <matricarin@gmail.com>
2025-03-12 18:59:53 +0500

    parser was fixed

--- Common/Common.TasksLibrary/Task2/Handlers/CommandParser.cs
+++ Common/Common.TasksLibrary/Task2/Handlers/CommandParser.cs
@@ -1,33 +1,40 @@
 ﻿namespace Common.TasksLibrary.Task2.Handlers;
 
-sealed class CommandParser
+public sealed class CommandParser
 {
     public string Command { get; }
     public string Arguments { get; }
 
     public CommandParser(string command)
     {
-        if (!string.IsNullOrWhiteSpace(command))
+        if (string.IsNullOrWhiteSpace(command))
         {
             throw new ArgumentException("Command cannot be empty.", nameof(command));
         }
-        
+
         var spannedCommand = command.AsSpan();
         var whiteSpaceIndex = spannedCommand.IndexOf(' ');
-        
+
         if (whiteSpaceIndex == -1)
         {
-            Command = spannedCommand.ToString();
+            Command = GetLineWithFirstUpperCharacter(spannedCommand);
             Arguments = string.Empty;
         }
         else
         {
-            spannedCommand.Slice(0, whiteSpaceIndex);
-            Command = spannedCommand.ToString();
-
-            spannedCommand.Slice(whiteSpaceIndex + 1);
-            Arguments = spannedCommand.ToString();
+            Command = GetLineWithFirstUpperCharacter(spannedCommand.Slice(0, whiteSpaceIndex));
+            Arguments = spannedCommand.Slice(whiteSpaceIndex + 1).ToString();
         }
-        
+    }
+
+    private string GetLineWithFirstUpperCharacter(ReadOnlySpan<char> input)
+    {
+        if (input.Length > 1)
+        {
+            return input.Slice(0, 1).ToString()
+                   + input.Slice(1, input.Length - 1).ToString().ToLower();
+        }
+
+        return input.ToString();
     }
 }
--- Tests/PracticeTests/Task2Tests/CommandParserTests.cs
+++ Tests/PracticeTests/Task2Tests/CommandParserTests.cs
@@ -1,12 +1,27 @@
-﻿namespace PracticeTests.Task2Tests;
+﻿using Common.TasksLibrary.Task2.Handlers;
+
+namespace PracticeTests.Task2Tests;
 
 [TestFixture]
 [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public sealed class CommandParserTests
 {
-    [Test]
-    public void ParseCommandTest()
+    private CommandParser? Parser { get; set; }
+    
+    [TestCase("+", "+", "")]
+    [TestCase("-", "-", "")]
+    [TestCase("*", "*", "")]
+    [TestCase("/", "/", "")]
+    [TestCase("DEFINE a 5", "Define", "a 5")]
+    [TestCase("PRINT", "Print", "")]
+    public void ParseCommandTest(string input, string command, string arguments)
     {
-        Assert.IsTrue(true);
+        Parser = new CommandParser(input);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(Parser.Command, Is.EqualTo(command));
+            Assert.That(Parser.Arguments, Is.EqualTo(arguments));
+        });
     }
 }
